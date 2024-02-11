616c349b6fc53331745e284ec87c723fe65caa33
Garrett M <molle_g@yahoo.com>
2024-02-11 05:02:37 -0500

    Feature: Implemented basic navbar and querying for boards as well as empty states

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,6 +4,7 @@ export const Loading = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       <Image
+        priority
         src="/logo.svg"
         alt="logo"
         width={120}
