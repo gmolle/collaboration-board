f6b98e6bd33e9131bede53da7f39b7bb4eef9fc7
Garrett M <molle_g@yahoo.com>
2024-02-11 09:17:09 -0500

    Feature: Implemented basic features of the board list

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
   const { organization } = useOrganization();
 
   return (
-    <div className="flex items-center gap-x-4 p-5">
+    <div className="flex items-center gap-x-4 p-5 sticky top-0 bg-white z-50">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -19,7 +19,7 @@ export const OrgSidebar = () => {
   const favorites = searchParams.get("favorites");
 
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 ">
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 sticky top-0 max-h-[50%]">
       <Link href="/">
         <div className="flex items-center gap-x-2">
           <Image src="/logo.svg" alt="Logo" height={60} width={60} />
