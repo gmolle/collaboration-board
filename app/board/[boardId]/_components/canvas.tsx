c7c7c75581247b7a3a66495cc9682b64e251b2ee
Garrett M <molle_g@yahoo.com>
2024-02-13 00:56:25 -0500

    Feature: Implemented Info component with specific info and actions related to the current board

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,8 +4,6 @@ import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
-import { useSelf } from "@/liveblocks.config";
-
 interface CanvasProps {
   boardId: string;
 }
@@ -13,7 +11,7 @@ interface CanvasProps {
 export const Canvas = ({ boardId }: CanvasProps) => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
