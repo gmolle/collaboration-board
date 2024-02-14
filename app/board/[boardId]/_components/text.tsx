75fc5fb87ae1e6150d6618e2a460e5feb5dd71d3
Garrett M <molle_g@yahoo.com>
2024-02-14 04:40:11 -0500

    Refactor: Reduced the base font size of the text layers slightly

--- app/board/[boardId]/_components/text.tsx
+++ app/board/[boardId]/_components/text.tsx
@@ -11,7 +11,7 @@ const font = Kalam({
 
 const calculateFontSize = (width: number, height: number) => {
   const maxFontSize = 96;
-  const scaleFactor = 0.5;
+  const scaleFactor = 0.35;
   const fontSizeBasedOnHeight = height * scaleFactor;
   const fontsizeBasedOnWidth = width * scaleFactor;
 
