f674ddd12a4272a1e260caa3e3fe294d09f89c3d
Garrett M <molle_g@yahoo.com>
2024-02-14 04:34:08 -0500

    Refactor: Fix warning about next image priority

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -60,7 +60,13 @@ export const BoardCard = ({
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
-          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Image
+            src={imageUrl}
+            alt={title}
+            fill
+            className="object-fit"
+            priority
+          />
           <Overlay />
           <Actions id={id} title={title} side="right">
             <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
